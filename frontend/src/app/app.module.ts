dec7001b62d57bbc39cc5e0508e15b5e71e18045
maghamohamadi <Aghamohamadi@gmail.com>
2023-01-19 23:27:32 +0100

    Frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskViewComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { TaskService } from 'src/app/task.service';
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+constructor(private taskService: TaskService) { }
+
+ngOnInit() {
+}
+
+createNewList(){
+  this.taskService.createList{'testing'}.subscribe((response:any) => {
+    console.log(response);
+  })
+}}
